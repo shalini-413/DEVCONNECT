5764d2e7cee0a57ce328526fdb96f246865bb83a
Shalini <shalinishalu462002@gmail.com>
2025-06-21 21:28:29 +0530

    static path reuired

--- devconnector-backend/index.js
+++ devconnector-backend/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
+const path = require("path");
+
 
 const authRoutes = require("./routes/authRoutes");
 const postRoutes = require("./routes/postRoutes");
